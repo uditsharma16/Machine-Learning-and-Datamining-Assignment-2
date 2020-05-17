1bb87861ff2457d06f4340e75938b7d06b05b132
uditsharma16 <uditsharma16@yahoo.com>
2020-05-17 16:35:05 +1200

    K-Means CLustering- Fixing the K means clustering

--- Task 1.py
+++ Task 1.py
@@ -1,27 +1,16 @@
 
 ########################################Part 1###########################
-# from sklearn.datasets import load_iris
-# X, y = load_iris(return_rawdata_y=True)
-#
-# # kmeans = KMeans(n_clusters=2).fit(rawdata)
-# # kmeans.labels_
 
 import numpy as np
 import pandas as pd
-
 from sklearn.datasets import make_blobs
 from sklearn.metrics import silhouette_samples
 from sklearn.metrics import silhouette_score
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import datetime
-
 from matplotlib.ticker import FixedLocator, FixedFormatter
 from pylab import rcParams
-
-
-
-
 
 #Importing data
 data_raw = pd.read_csv(r"C:\Users\udit sharma\Desktop\Aut\Data Mining and Machine Learning\Assignment 2 Dataset\Dow Jones Index.csv")
@@ -39,12 +28,12 @@ from sklearn.preprocessing import LabelEncoder
 label_encoder = LabelEncoder()
 data_raw['stock']= label_encoder.fit_transform(data_raw['stock'])
 data_raw=data_raw.drop(columns='date')
+
 #importing Kmeans clustering algorithm
-
 from sklearn.cluster import KMeans
-
 data_raw_kmeans = KMeans(n_clusters=3)
 data_raw_kmeans.fit(data_raw)
 data_raw.shape
 data_raw_kmeans.cluster_centers_
+
 
