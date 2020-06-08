d8c628621d507fb40d0a857a03e7e5ac816ab483
uditsharma16 <uditsharma16@yahoo.com>
2020-06-08 15:35:09 +1200

    Adding Sum Square Error Funtions for DBSCAN And Agglomerative clustering. Finding the CSM plots for all 4 datasets

--- Task 1- Dow Jones.py
+++ Task 1- Dow Jones.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[173]:
+# In[21]:
 
 
 import numpy as np
@@ -38,7 +38,7 @@ data_raw=data_raw.drop(columns='date')
 data_raw.head()
 
 
-# In[174]:
+# In[22]:
 
 
 #Scaling the Data
@@ -52,7 +52,7 @@ data_scale=scaler.transform(data_raw)
 print(data_scale)
 
 
-# In[190]:
+# In[23]:
 
 
 #Applying PCA to Data
@@ -63,7 +63,7 @@ print(data_dow)
 plt.scatter(data_dow[:,0], data_dow[:,1])
 
 
-# In[192]:
+# In[24]:
 
 
 # importing Kmeans clustering algorithm
@@ -90,23 +90,23 @@ _ = plt.legend(shadow=True)
 _ = plt.show()
 
 
-# In[177]:
+# In[25]:
 
 
 data_dow_km = KMeans(n_clusters=9)
-start_dow = time.time()
+k_start_dow = time.time()
 data_dow_kmfit=data_dow_km.fit(data_dow)
+k_end_dow = time.time()
 data_dow_kmeans=data_dow_km.fit_predict(data_dow)
-end_dow = time.time()
-timet_dow=end_dow-start_dow
+k_timet_dow=k_end_dow-k_start_dow
 # data_dow.shape
 # data_dow_kmeans.cluster_centers_
 
 
-# In[186]:
+# In[26]:
 
 
-print("Time taken by the KMeans algorithm:-",timet_dow)
+print("Time taken by the KMeans algorithm:-",k_timet_dow)
 print("The sum of square errors is:-",data_dow_kmfit.inertia_)
 # data_dow_clustered = pd.concat([data_dow,pd.Series(data_dow_kmeans.labels_)],axis=1)
 # data_dow_clustered.rename(columns={data_dow_clustered.columns[-1]:'Cluster_number'},inplace=True)
@@ -116,7 +116,7 @@ silhoute_score = silhouette_score(data_dow, data_dow_kmfit.labels_)
 print("The silhoute score is:-",silhoute_score)
 
 
-# In[217]:
+# In[ ]:
 
 
 data_color=['red','green','blue','yellow','orange','violet','pink','grey','black']
@@ -128,6 +128,11 @@ for i in range(num_opt_clus):
             marker='s', edgecolor='black',
             label='cluster '+str(i+1))
 
+plt.scatter(data_dow_kmfit.cluster_centers_[:, 0],
+            data_dow_kmfit.cluster_centers_[:, 1],
+            s=250, marker='*',
+            c='red', edgecolor='black',
+            label='centroids')
 plt.legend(scatterpoints=1)
 plt.grid()
 plt.tight_layout()
@@ -135,7 +140,7 @@ plt.tight_layout()
 plt.show()
 
 
-# In[180]:
+# In[28]:
 
 
 
@@ -167,13 +172,46 @@ plt.tight_layout()
 plt.show()
 
 
-# In[240]:
+# In[43]:
+
+
+start= 0.0
+stop= 1.0
+step = 0.01
+my_list = np.arange(start, stop+step, step)
+# print(my_list)
+sildbscan=[]
+plter=[]
+max_eps=0.1
+max_silscore=0
+for i in range(80):
+    db = DBSCAN(eps=.05+my_list[i],min_samples=4)
+    data_dbscan = db.fit(data_dow)
+    labels = db.labels_
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+    if(n_clusters_>0):
+     sil_score=metrics.silhouette_score(data_dow,labels)
+     sildbscan.append(sil_score)
+     plter.append(i)
+     if(sil_score>max_silscore):
+            max_silscore=sil_score
+            max_eps=0.05+my_list[i]
+# print(sildbscan)
+print("Optimal eps for DBSCAN is:",max_eps)
+plt.plot(plter,sildbscan)
+plt.show()
+
+
+# In[49]:
 
 
 from sklearn.cluster import DBSCAN
 from sklearn import metrics
-db=DBSCAN(eps=.27, min_samples=4)
+db=DBSCAN(eps=.77, min_samples=4)
+db_start_time=time.time()
 data_dbscan=db.fit(data_dow)
+db_end_time=time.time()
+db_timet=db_end_time-db_start_time
 data_dbs=db.fit_predict(data_dow)
 # core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
 # core_samples_mask[db.core_sample_indices_] = True
@@ -183,14 +221,26 @@ n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 n_noise_ = list(labels).count(-1)
 print('Estimated number of clusters: %d' % n_clusters_)
 print('Estimated number of noise points: %d' % n_noise_)
+print("Time Taken by DBSCAN Algorithm:-",db_timet)
 print("Silhouette Coefficient: %0.3f"% metrics.silhouette_score(data_dow, labels))
 data_color=['red','green','blue','yellow','orange','violet','pink','grey','black','purple']
-for i in range(-1,num_opt_clus):  
- plt.scatter(data_dow[data_dbs == i,0],
+# for i in range(-1,num_opt_clus):  
+#  plt.scatter(data_dow[data_dbs == i,0],
+#             data_dow[data_dbs == i,1],
+#             s=50, c=data_color[i],
+#             marker='s', edgecolor='black',
+#             label='cluster '+str(i+1))
+for i in range(-1,n_clusters_):  
+  plt.scatter(data_dow[data_dbs == i,0],
             data_dow[data_dbs == i,1],
             s=50, c=data_color[i],
             marker='s', edgecolor='black',
             label='cluster '+str(i+1))
+# plt.scatter(data_dow[data_dbs == -1,0],
+#             data_dow[data_dbs == -1,1],
+#             s=50, c=data_color[0],
+#             marker='s', edgecolor='black',
+#             label='cluster '+str(1))
 
 plt.legend(scatterpoints=1)
 plt.grid()
@@ -199,7 +249,7 @@ plt.tight_layout()
 plt.show()
 
 
-# In[247]:
+# In[50]:
 
 
 cluster_labels = np.unique(data_dbs)
@@ -230,6 +280,103 @@ plt.xlabel('Silhouette coefficient')
 plt.tight_layout()
 #plt.savefig('06_06.png', dpi=300)
 plt.show()
+
+
+# In[51]:
+
+
+from sklearn.cluster import AgglomerativeClustering
+# finding optimal Value of K in Kmeans Clustering
+# K means per value of K
+n_clust_per_n = [AgglomerativeClustering(n).fit(data_dow) for n in range(1,11)]
+# print(k_means_per_k)
+silhoute_scores_per_n = [silhouette_score(data_dow,model.labels_) for model in n_clust_per_n[1:]]
+# print(silhoute_scores_per_n)
+# ploting graph for silhoute_scores
+rcParams['figure.figsize']=16,5
+_ = plt.plot(range(2,11),silhoute_scores_per_n,'bo-',color='blue',linewidth=3,markersize=8,label='silhoute Curve')
+_ = plt.xlabel('K',fontsize=14,family='Arial')
+_ = plt.ylabel('Silhoute Score',fontsize=14,family='Arial')
+_ = plt.grid(which='major',color='#cccccc',linestyle='--')
+_ = plt.title('Silhoute curve for predicting optimal number of clusters', family='Arial',fontsize=14)
+n=silhoute_scores_per_n.index(max(silhoute_scores_per_n))
+num_opt_clus=n+2
+#line to mark optimal number of k in curve
+_ = plt.axvline(x=n+2, linestyle='--', c='green',linewidth=3,label='Optimal Number of cluster ({})'.format(k))
+_ = plt.scatter(n+2, silhoute_scores_per_n[n],c='red',s=400)
+_ = plt.legend(shadow=True)
+_ = plt.show()
+
+
+# In[52]:
+
+
+ac = AgglomerativeClustering(n_clusters=9,
+                             affinity='euclidean',
+                             linkage='complete')
+ac_start_time=time.time()
+ac_fit= ac.fit(data_dow)
+ac_end_time=time.time()
+ac_labels = ac.fit_predict(data_dow)
+# print('Cluster labels: %s' % labels)
+ac_timet=ac_end_time-ac_start_time
+print("Time taken by agglomerative clustering:-",ac_timet)
+
+# sse=ac_fit.score(data_dow)
+# # print("The sum of squre errors:-",sse)
+# SSE = np.absolute(sse)
+# print("The sum of square errors is:-",SSE)
+# data_dow_clustered = pd.concat([data_dow,pd.Series(ac.labels_)],axis=1)
+# data_dow_clustered.rename(columns={data_dow_clustered.columns[-1]:'Cluster_number'},inplace=True)
+# data_dow_clustered.describe(include='all')
+# data_dow_clustered.sort_values(['Cluster_number'])
+silhoute_score = silhouette_score(data_dow, ac.labels_)
+print("The silhoute score is:",silhoute_score)
+
+
+# In[53]:
+
+
+cluster_labels = np.unique(ac_labels)
+n_clusters = cluster_labels.shape[0]
+silhouette_vals = silhouette_samples(data_dow, ac_labels, metric='euclidean')
+y_ax_lower, y_ax_upper = 0, 0
+yticks = []
+for i, c in enumerate(cluster_labels):
+    c_silhouette_vals = silhouette_vals[ac_labels == c]
+    c_silhouette_vals.sort()
+    y_ax_upper += len(c_silhouette_vals)
+    color = cm.jet(float(i) / n_clusters)
+#     if(c_silhouette_vals[i]>=0):
+    plt.barh(range(y_ax_lower, y_ax_upper), c_silhouette_vals, height=1.0,
+             edgecolor='none', color=color)
+    yticks.append((y_ax_lower + y_ax_upper) / 2.)
+    y_ax_lower += len(c_silhouette_vals)
+
+     
+
+silhouette_avg = np.mean(silhouette_vals)
+plt.axvline(silhouette_avg, color="red", linestyle="--")
+
+plt.yticks(yticks, cluster_labels + 1)
+plt.ylabel('Cluster')
+plt.xlabel('Silhouette coefficient')
+
+plt.tight_layout()
+#plt.savefig('06_06.png', dpi=300)
+plt.show()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
--- Task 2.py
+++ Task 2.py
@@ -1,7 +1,381 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[13]:
+
+
+import numpy as np
+import pandas as pd
+from sklearn.manifold import TSNE
+from sklearn.datasets import make_blobs
+from sklearn.metrics import silhouette_samples
+from sklearn.metrics import silhouette_score
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import datetime
+from matplotlib.ticker import FixedLocator, FixedFormatter
+from pylab import rcParams
+import time
+
+
+#Importing data
+# data_raw = pd.read_csv(r"C:\Users\udit sharma\Desktop\Aut\Data Mining and Machine Learning\Assignment 2 Dataset\water-treatment.csv")
+data_raw_st = pd.read_csv(r"C:\Users\udit sharma\Desktop\Aut\Data Mining and Machine Learning\Assignment 2 Dataset\Sales_Transactions_Dataset_Weekly.csv")
+data_raw_st.head()
+
+
+# In[14]:
+
+
+data_raw_st=data_raw_st.drop(columns='Product_Code')
+#Scaling the Data
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+scaler = StandardScaler()
+# scaler = MinMaxScaler()
+scaler.fit(data_raw_st)
+data_scale_st=scaler.transform(data_raw_st)
+# data_scale=data_raw
+# print(data_scale_st)
+#Applying PCA to Data
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2)
+data_st=pca.fit_transform(data_scale_st)
+# print(data_st)
+plt.scatter(data_st[:,0], data_st[:,1])
+
+
+# In[15]:
+
+
+data_raw_wt = pd.read_csv(r"C:\Users\udit sharma\Desktop\Aut\Data Mining and Machine Learning\Assignment 2 Dataset\water-treatment.csv")
+data_raw_wt.head()
+
+
+# In[16]:
+
+
+data_raw_wt=data_raw_wt.drop(columns='title')
+data_raw_wt.head()
+
+# Get ndArray of all column names 
+data_raw_wt.columns.values
+nrow,ncol=data_raw_wt.shape
+for col_name in list(data_raw_wt.columns.values):
+    data_raw_wt[col_name]=data_raw_wt[col_name].fillna(data_raw_wt[col_name].mean())
+
+
+# In[17]:
+
+
+scaler = StandardScaler()
+# scaler = MinMaxScaler()
+scaler.fit(data_raw_wt)
+data_scale_wt=scaler.transform(data_raw_wt)
+#Applying PCA to Data
+pca = PCA(n_components=2)
+data_wt=pca.fit_transform(data_scale_wt)
+# print(data_st)
+plt.scatter(data_wt[:,0], data_wt[:,1])
+
+
+# In[18]:
+
+
+data_raw_live = pd.read_csv(r"C:\Users\udit sharma\Desktop\Aut\Data Mining and Machine Learning\Assignment 2 Dataset\live.csv")
+data_raw_live.head()
+
+
+# In[19]:
+
+
+data_raw_live=data_raw_live.drop(columns='status_id')
+data_raw_live=data_raw_live.drop(columns='status_type')
+data_raw_live=data_raw_live.drop(columns='status_published')
+data_raw_live=data_raw_live.drop(columns='Column1')
+data_raw_live=data_raw_live.drop(columns='Column2')
+data_raw_live=data_raw_live.drop(columns='Column3')
+data_raw_live=data_raw_live.drop(columns='Column4')
+
+
+# In[20]:
+
+
+scaler = StandardScaler()
+# scaler = MinMaxScaler()
+scaler.fit(data_raw_live)
+data_scale_live=scaler.transform(data_raw_live)
+#Applying PCA to Data
+pca = PCA(n_components=2)
+data_live=pca.fit_transform(data_scale_live)
+plt.scatter(data_live[:,0], data_live[:,1])
+
+
+# In[21]:
+
+
+#Importing data
+data_raw = pd.read_csv(r"C:\Users\udit sharma\Desktop\Aut\Data Mining and Machine Learning\Assignment 2 Dataset\Dow Jones Index.csv")
+
+#Cleaning the Data
+data_raw.describe(include='all')
+data_raw.isnull().sum()
+data_raw.percent_change_volume_over_last_wk = data_raw.percent_change_volume_over_last_wk.fillna(data_raw.percent_change_volume_over_last_wk.mean())
+data_raw.previous_weeks_volume = data_raw.previous_weeks_volume.fillna(data_raw.previous_weeks_volume.mean())
+data_raw.head()
+# data_raw.columns[3:7]
+data_raw[data_raw.columns[3:7]] = data_raw[data_raw.columns[3:7]].replace('[\$,]', '', regex=True).astype(float)
+data_raw[data_raw.columns[11:13]] = data_raw[data_raw.columns[11:13]].replace('[\$,]', '', regex=True).astype(float)
+data_raw.head()
+from sklearn.preprocessing import LabelEncoder
+label_encoder = LabelEncoder()
+data_raw['stock']= label_encoder.fit_transform(data_raw['stock'])
+data_raw=data_raw.drop(columns='date')
+data_raw.head()
+scaler = StandardScaler()
+# scaler = MinMaxScaler()
+scaler.fit(data_raw)
+data_scale=scaler.transform(data_raw)
+# data_scale=data_raw
+# print(data_scale)
+#Applying PCA to Data
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2)
+data_dow=pca.fit_transform(data_scale)
+# print(data_dow)
+plt.scatter(data_dow[:,0], data_dow[:,1])
+
+
+# In[22]:
+
+
+from sklearn.cluster import KMeans
+data_set_list=[data_dow,data_live,data_st,data_wt]
+data_set_names=["Dow Jones dataset","Facebook Live dataset","Sales and Transactions Dataset","Water Treatment Dataset"]
+optimum_k=[9,9,9,9]
+optimum_eps=[0.71,0.71,0.71,0.71]
+optimum_n=[9,9,9,9]
+def calClusterCenters(data_set_sample,data_set_labels):
+    clustcord=[]
+    clus_list=[]
+    for i in range(len(data_set_sample)):
+        if(data_set_labels[i] not in clus_list):
+            clus_list.append(data_set_labels[i])
+    for i in clus_list:
+        subclus=[]
+        x_cord=sum(data_set_sample[data_set_labels==i,0])
+        y_cord=sum(data_set_sample[data_set_labels==i,1])
+        count=0
+        for j in range(len(data_set_sample)):
+            if(data_set_labels[j]==i):
+                count+=1
+        clustcord.append([x_cord/count,y_cord/count,i])
+    return clustcord
+
+def sumSquareError(data_set_sample,data_set_labels):
+    sse=0
+    data_set_centroids=calClusterCenters(data_set_sample,data_set_labels)
+    for i in range(len(data_set_sample)):
+        k=0
+        for j in range(len(data_set_centroids)):
+            if(data_set_centroids[j][2]==data_set_labels[i]):
+                k=j
+        x_dist=data_set_centroids[k][0]-data_set_sample[i,0]
+        y_dist=data_set_centroids[k][1]-data_set_sample[i,1]
+        total_dist=(x_dist*x_dist+y_dist*y_dist)
+        sse+=total_dist
+    return sse
+
+
+# In[209]:
+
+
+from matplotlib import cm
+from tabulate import tabulate
+tablelistkm=[]
+fig, ay = plt.subplots(2,2,figsize=(10,10))
+ax=[ay[0][0],ay[0][1],ay[1][0],ay[1][1]]
+for i,data_set in enumerate(data_set_list):
+    data_set_km= KMeans(n_clusters=optimum_k[i])
+    data_set_stime=time.time()
+    data_set_kmfit=data_set_km.fit(data_set)
+    data_set_etime=time.time()
+    data_set_kmfp=data_set_km.fit_predict(data_set)
+    sse=data_set_kmfit.inertia_
+    silhoute_score_dataset = silhouette_score(data_set, data_set_kmfit.labels_)
+    data_set_ttime=data_set_etime-data_set_stime
+    tablist=[data_set_names[i],silhoute_score_dataset,sumSquareError(data_set,data_set_kmfit.labels_),data_set_ttime]
+    tablelistkm.append(tablist)
+    
+    cluster_labels = np.unique(data_set_kmfp)
+    n_clusters = cluster_labels.shape[0]
+    silhouette_vals = silhouette_samples(data_set, data_set_kmfp, metric='euclidean')
+    y_ax_lower, y_ax_upper = 0, 0
+    yticks = []
+    for x, c in enumerate(cluster_labels):
+     c_silhouette_vals = silhouette_vals[data_set_kmfp == c]
+     c_silhouette_vals.sort()
+     y_ax_upper += len(c_silhouette_vals)
+     color = cm.jet(float(x) / n_clusters)
+     ax[i].barh(range(y_ax_lower, y_ax_upper), c_silhouette_vals, height=1.0,
+             edgecolor='none', color=color)
+
+     yticks.append((y_ax_lower + y_ax_upper) / 2.)
+     y_ax_lower += len(c_silhouette_vals)
+    silhouette_avg = np.mean(silhouette_vals)
+    ax[i].axvline(silhouette_avg, color="red", linestyle="--")
+    ax[i].set_title(data_set_names[i])
+fig.suptitle("The CSM Plot for Different Dataset for KMeans", fontsize=14)  
+plt.show()
+print("The results for Kmeans are as follows:-")
+print(tabulate(tablelistkm, headers=['Data Set','Silhouette Score','Sum Square Error','Time Taken']))    
+
+
+# In[206]:
+
+
+from sklearn.cluster import DBSCAN
+tablelistdb=[]
+fig, ay = plt.subplots(2,2,figsize=(10,10))
+ax=[ay[0][0],ay[0][1],ay[1][0],ay[1][1]]
+for i,data_set in enumerate(data_set_list):
+    data_set_dbscan=DBSCAN(eps=optimum_eps[i],min_samples=4)
+    db_stime=time.time()
+    data_dbfit=data_set_dbscan.fit(data_set)
+    db_etime=time.time()
+    data_dbfp=data_set_dbscan.fit_predict(data_set)
+    silhoute_score_dataset = silhouette_score(data_set, data_dbfit.labels_)
+    data_db_ttime=db_etime-db_stime
+#     db_sse=sumSqaureError(data_set,data_dbfit.labels_,data_dbfir.cluster_centers_)
+    tablist=[data_set_names[i],silhoute_score_dataset,sumSquareError(data_set,data_dbfit.labels_),data_db_ttime]
+    tablelistdb.append(tablist)
+    
+   
+    cluster_labels = np.unique(data_dbfp)
+    n_clusters = cluster_labels.shape[0]
+    silhouette_vals = silhouette_samples(data_set, data_dbfp, metric='euclidean')
+    y_ax_lower, y_ax_upper = 0, 0
+    yticks = []
+    for x, c in enumerate(cluster_labels):
+     c_silhouette_vals = silhouette_vals[data_dbfp == c]
+     c_silhouette_vals.sort()
+     y_ax_upper += len(c_silhouette_vals)
+     color = cm.jet(float(x) / n_clusters)
+     ax[i].barh(range(y_ax_lower, y_ax_upper), c_silhouette_vals, height=1.0,
+             edgecolor='none', color=color)
+
+     yticks.append((y_ax_lower + y_ax_upper) / 2.)
+     y_ax_lower += len(c_silhouette_vals)
+    silhouette_avg = np.mean(silhouette_vals)
+    ax[i].axvline(silhouette_avg, color="red", linestyle="--")
+    ax[i].set_title(data_set_names[i])
+fig.suptitle("The CSM Plot for Different Dataset for DBSCAN Clustering", fontsize=14)  
+plt.show()
+print("The results for DBSCAN are as follows:-")
+print(tabulate(tablelistdb, headers=['data set','Silhouette Score','Sum Square Error','Time Taken']))    
+
+
+# In[207]:
+
+
+from sklearn.cluster import AgglomerativeClustering
+tablelistac=[]
+fig, ay = plt.subplots(2,2,figsize=(10,10))
+ax=[ay[0][0],ay[0][1],ay[1][0],ay[1][1]]
+for i,data_set in enumerate(data_set_list):
+    data_set_ac=AgglomerativeClustering(optimum_n[i])
+    ac_stime=time.time()
+    data_acfit=data_set_ac.fit(data_set)
+    ac_etime=time.time()
+    data_acfp=data_set_ac.fit_predict(data_set)
+    silhoute_score_dataset = silhouette_score(data_set, data_acfit.labels_)
+    data_ac_ttime=ac_etime-ac_stime
+    tablist=[data_set_names[i],silhoute_score_dataset,sumSquareError(data_set,data_acfit.labels_),data_ac_ttime]
+    tablelistac.append(tablist)
+    
+    cluster_labels = np.unique(data_acfp)
+    n_clusters = cluster_labels.shape[0]
+    silhouette_vals = silhouette_samples(data_set, data_acfp, metric='euclidean')
+    y_ax_lower, y_ax_upper = 0, 0
+    yticks = []
+    for x, c in enumerate(cluster_labels):
+     c_silhouette_vals = silhouette_vals[data_acfp == c]
+     c_silhouette_vals.sort()
+     y_ax_upper += len(c_silhouette_vals)
+     color = cm.jet(float(x) / n_clusters)
+     ax[i].barh(range(y_ax_lower, y_ax_upper), c_silhouette_vals, height=1.0,
+             edgecolor='none', color=color)
+
+     yticks.append((y_ax_lower + y_ax_upper) / 2.)
+     y_ax_lower += len(c_silhouette_vals)
+    silhouette_avg = np.mean(silhouette_vals)
+    ax[i].axvline(silhouette_avg, color="red", linestyle="--")
+    ax[i].set_title(data_set_names[i])
+fig.suptitle("The CSM Plot for Different Dataset for DBSCAN Clustering", fontsize=14)  
+plt.show()
+print("The results for Agglomerative Clustering are as follows:-")
+print(tabulate(tablelistac, headers=['data set','Silhouette Score','Sum of Square Error','Time Taken']))  
+
+
+# In[194]:
+
+
+# # def sumSquareError():
+# data_set_km= KMeans(n_clusters=9)
+# data_dow_kmfit=data_set_km.fit(data_dow)
+# print(data_dow_kmfit.labels_)
+# print(calClusterCenters(data_dow,data_dow_kmfit.labels_))
+# # print(data_dow_kmfit.labels_)
+# print(data_set_kmfit.cluster_centers_)
+# data_dow_kmeans=data_set_km.fit_predict(data_dow)
+# # print(data_set_kmfit.cluster_centers_)
+# # print(len(data_dow))
+# # print(data_dow)
+
+# data_color=['lightblue','green','blue','yellow','orange','violet','pink','grey','black']
+
+# for i in range(9):  
+#  plt.scatter(data_dow[data_dow_kmeans == i,0],
+#             data_dow[data_dow_kmeans == i,1],
+#             s=50, c=data_color[i],
+#             marker='s', edgecolor='black',
+#             label='cluster '+str(i+1))
+# cust_center=data_dow_kmfit.cluster_centers_
+# labels=data_set_kmfit.labels_
+# # print(cust_center)
+# # plt.scatter(data_dow_kmfit.cluster_centers_[:, 0],
+# #             data_dow_kmfit.cluster_centers_[:, 1],
+# #             s=250, marker='*',
+# #             c='red', edgecolor='black',
+# #             label='centroids')
+# # for i in range(len(data_dow_kmfit.cluster_centers_)):
+# plt.scatter(data_dow_kmfit.cluster_centers_[:, 0],
+#             data_dow_kmfit.cluster_centers_[:, 1],
+#             s=250, marker='*',
+#             c='red', edgecolor='black',
+#             label='centroids'+str(i+1))
+# plt.legend(scatterpoints=1)
+# plt.grid()
+# plt.tight_layout()
+# #plt.savefig('06_02.png', dpi=300)
+# plt.show()
+# print("The sum of square errors is:-",data_dow_kmfit.score(data_dow))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
 
 
 
